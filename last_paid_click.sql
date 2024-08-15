0a61bc2f873a1c42c36699593d6ff2098e1feef5
myuzer <165842087+myuzer@users.noreply.github.com>
2024-08-15 21:25:45 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,60 +1,42 @@
 WITH paid_sessions AS (
     SELECT
-        visitor_id,
-        visit_date,
-        source,
-        medium,
-        campaign
+        *,
+        ROW_NUMBER()
+            OVER (PARTITION BY visitor_id ORDER BY visit_date DESC)
+        AS rn
     FROM sessions
     WHERE medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-),
-
-last_paid_click AS (
-    SELECT
-        visitor_id,
-        source AS utm_source,
-        medium AS utm_medium,
-        campaign AS utm_campaign,
-        MAX(visit_date) AS visit_date
-    FROM paid_sessions
-    GROUP BY
-        visitor_id,
-        utm_source,
-        utm_medium,
-        utm_campaign
-    ORDER BY
-        visitor_id,
-        MAX(visit_date) DESC
-),
-
-lead_data AS (
-    SELECT
-        visitor_id,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id
-    FROM leads
 )
 
 SELECT
-    lpc.visitor_id,
-    lpc.visit_date,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
-    ld.lead_id,
-    ld.created_at,
-    ld.amount,
-    ld.closing_reason,
-    ld.status_id
-FROM last_paid_click AS lpc
-LEFT JOIN lead_data AS ld
-    ON lpc.visitor_id = ld.visitor_id
+    ps.visitor_id,
+    ps.visit_date,
+    ps.source AS utm_source,
+    ps.medium AS utm_medium,
+    ps.campaign AS utm_campaign,
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+FROM paid_sessions AS ps
+LEFT JOIN leads AS l
+    ON ps.visitor_id = l.visitor_id
+WHERE
+    (
+        ps.rn = 1
+        AND ps.visit_date <= l.created_at
+    )
+    OR (
+        ps.rn = 1
+        AND l.created_at IS NULL
+    )
 ORDER BY
-    ld.amount DESC NULLS LAST,
-    lpc.visit_date ASC,
-    lpc.utm_source ASC,
-    lpc.utm_medium ASC,
-    lpc.utm_campaign ASC;
+    l.amount DESC NULLS LAST,
+    ps.visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC;
+
+
+
